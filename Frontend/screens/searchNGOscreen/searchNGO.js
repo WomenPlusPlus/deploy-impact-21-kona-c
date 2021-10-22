d838f9dbdbe2b50a4520ab63beeafcae336d69b4
justina <justynazurawska@icloud.com>
2021-10-22 19:05:02 +0200

    add search box

--- Frontend/screens/searchNGOscreen/searchNGO.js
+++ Frontend/screens/searchNGOscreen/searchNGO.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { View, Text,Image } from 'react-native';
+import React, {useState} from "react";
+import { View, Text, Image, TextInput } from 'react-native';
 import { styles } from "./searchNGOstyles";
 
 const Search = () => {
 
+    const [searchTerm, setSearchTerm] = useState('')
+
     return (
         <View style={styles.container}>
-            <View>
             <Text style={styles.title}>FIND HELP NEAR YOU</Text>
-            
+
             <Image 
             style={styles.image}
             source={{ 
                 uri: 'https://picsum.photos/200/300'}}/>
 
-            
-            </View>
+            <TextInput 
+            placeholder= 'Search...'
+            style={styles.search}
+            defaultValue={searchTerm}/>
+  
         </View>
     );
 };
--- Frontend/screens/searchNGOscreen/searchNGOstyles.js
+++ Frontend/screens/searchNGOscreen/searchNGOstyles.js
@@ -5,7 +5,8 @@ export const styles = StyleSheet.create({
     container: { 
         flex: 1, 
         alignItems: 'center', 
-        justifyContent: 'flex-start' 
+        justifyContent: 'flex-start',
+        backgroundColor: 'white', 
     },
     title:{
         fontWeight: 'bold',
@@ -17,5 +18,16 @@ export const styles = StyleSheet.create({
         width: 250,
         height: 150, 
         alignSelf: 'center',   
+    },
+    search:{
+        padding: 15, 
+        marginTop:20,
+        alignItems: "center", 
+        borderRadius: 25,
+        borderColor: 'whitesmoke',
+        borderStyle: 'solid',
+        borderWidth: 2,
+        width: '70%',
+
     }    
     });
