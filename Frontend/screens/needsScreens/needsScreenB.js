b6728d91821c276f2b48f419bfecff931a8ef316
Natayra Pacheco dos Santos <natayras@gmail.com>
2021-11-05 10:12:07 +0100

    Created files/folders

--- Frontend/screens/needsScreens/needsScreenB.js
+++ Frontend/screens/needsScreens/needsScreenB.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, ScrollView, View, Pressable } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
-const NeedsScreen1 = ({ route, navigation }) => {
+const NeedsScreenB = ({ route, navigation }) => {
   return (
     <ScrollView>
       <Text>What kind of help do you need?</Text>
@@ -10,4 +10,4 @@ const NeedsScreen1 = ({ route, navigation }) => {
   );
 };
 
-export default NeedsScreen1;
+export default NeedsScreenB;
---
+++ Frontend/screens/needsScreens/needsScreenC.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, ScrollView, View, Pressable } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+const NeedsScreenC = ({ route, navigation }) => {
+  return (
+    <ScrollView>
+      <Text>What kind of help do you need?</Text>
+    </ScrollView>
+  );
+};
+
+export default NeedsScreenC;
