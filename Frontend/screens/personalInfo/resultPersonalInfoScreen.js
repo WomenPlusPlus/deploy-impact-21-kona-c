3ec744031d18b2502eb563a570cae5fe08601eb5
justina <justynazurawska@icloud.com>
2021-11-07 16:47:52 +0100

    code revie: remove user data as not used

--- Frontend/screens/personalInfo/resultPersonalInfoScreen.js
+++ Frontend/screens/personalInfo/resultPersonalInfoScreen.js
@@ -7,7 +7,7 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./stylesPInfo";
-import UserData from "../../components/userData";
+
 
 // this is a future feature where we return user data gathered from the personal information
 // based on your answers, we found help for you
@@ -16,7 +16,7 @@ const ResultInfoScreen = ({ navigation }) => {
     <SafeAreaView>
       <ScrollView>
         <View style={styles.container}>
-          {/* <UserData />  */}
+
           <Text>hello i am a new screen</Text>
         </View>
       </ScrollView>
