7cef66a024d00df474a9ab3d8b071d88e54acf25
justina <justynazurawska@icloud.com>
2021-11-07 16:50:27 +0100

    code revie: remove resultPersonalInfoScreen  as not used

--- Frontend/screens/routes/stackNavigator.js
+++ Frontend/screens/routes/stackNavigator.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import Development from "../development/developmentScreen";
 import HomeScreen from "../welcomeScreen/welcomeScreen";
 import OrganisationsListScreen from "../organisationsListScreen/organisationsListScreen";
