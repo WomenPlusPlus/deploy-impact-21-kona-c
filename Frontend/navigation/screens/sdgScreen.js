94c029a8723ea0192c1f336a4a12f6f1ae928c85
Natayra Pacheco dos Santos <natayras@gmail.com>
2021-11-11 21:18:25 +0100

    sgds

--- Frontend/navigation/screens/sdgScreen.js
+++ Frontend/navigation/screens/sdgScreen.js
@@ -8,86 +8,26 @@ import {
   TouchableOpacity,
   ScrollView,
   ImageBackground,
+  Image,
   View,
   Pressable,
 } from "react-native";
 import normalize from "react-native-normalize";
-import data from "../../assets/organisations.json";
 import {
   FontAwesome,
   Ionicons,
   MaterialCommunityIcons,
 } from "@expo/vector-icons";
+import data from "../../assets/organisations.json";
 
 const sdgs = [
   {
     id: 1,
-    title: "1. No poverty",
+    image: require("../../assets/E-WEB-Goal-01.png"),
   },
   {
     id: 2,
-    title: "2. Zero hunger",
-  },
-  {
-    id: 3,
-    title: "3. Good health and well-being",
-  },
-
-  {
-    id: 4,
-    title: "4. Quality education",
-  },
-  {
-    id: 5,
-    title: "5. Gender equality",
-  },
-  {
-    id: 6,
-    title: "6. Clean water and sanitation",
-  },
-  {
-    id: 7,
-    title: "7. Affordable and clean energy",
-  },
-  {
-    id: 8,
-    title: "8. Decent work and economic growth",
-  },
-  {
-    id: 9,
-    title: "9. Industry, innovation and infrastructure",
-  },
-  {
-    id: 10,
-    title: "10. Reduced inequalities",
-  },
-  {
-    id: 11,
-    title: "11. Sustainable cities and communities",
-  },
-  {
-    id: 12,
-    title: "12. Responsible consumption and production",
-  },
-  {
-    id: 13,
-    title: "13. Climate action",
-  },
-  {
-    id: 14,
-    title: "14. Life below water",
-  },
-  {
-    id: 15,
-    title: "15. Life on land",
-  },
-  {
-    id: 16,
-    title: "16. Peace, justice and strong institutions",
-  },
-  {
-    id: 17,
-    title: "17. Partnerships for the goals",
+    image: require("../../assets/E-WEB-Goal-02.png"),
   },
 ];
 
@@ -116,90 +56,27 @@ const SdgScreen = () => {
       style={styles.image}
     >
       <ScrollView>
-        {sdgs.map((sdg) => (
-          <View style={styles.container}>
-            <Pressable
-              style={styles.button}
-              onPress={() => handlePress(sdg.id)}
-            >
-              <Text style={styles.buttonText}>{sdg.title}</Text>
-            </Pressable>
-          </View>
-        ))}
+        <View style={styles.container}>
+          {sdgs.map((sdg) => (
+            <View style={styles.buttonContainer}>
+              <Pressable
+                style={styles.button}
+                onPress={() => handlePress(sdg.id)}
+              >
+                <Image
+                  resizeMode="contain"
+                  source={sdg.image}
+                  style={styles.im}
+                />
+              </Pressable>
+            </View>
+          ))}
+        </View>
         {newData.map((organisation) => (
           <View key={"listview" + organisation} style={styles.container}>
             <Text key={"name" + organisation} style={styles.textBoxName}>
               {data[organisation]["Name of Organisation"]}
             </Text>
-            {data[organisation]["Address"] === "" ? null : (
-              <Text key={"address" + organisation} style={styles.flexContainer}>
-                <Ionicons style={styles.iconText} name="home" />
-                <Text
-                  style={styles.textBox}
-                >{` ${data[organisation]["Address"]}`}</Text>
-              </Text>
-            )}
-            <Text key={"region" + organisation} style={styles.flexContainer}>
-              <Ionicons style={styles.iconText} name="location" />
-              <Text style={styles.textBox}>
-                {data[organisation]["Region"] === ""
-                  ? `Worldwide`
-                  : ` ${data[organisation]["Region"]}`}
-              </Text>
-            </Text>
-            {data[organisation]["Web Address"] === "" ? null : (
-              <Text key={"website" + organisation} style={styles.flexContainer}>
-                <MaterialCommunityIcons style={styles.iconText} name="web" />
-                <Text style={styles.textBox}>{` `}</Text>
-                <Text
-                  style={styles.textBoxWebsite}
-                  onPress={() =>
-                    Linking.openURL(data[organisation]["Web Address"])
-                  }
-                >
-                  {data[organisation]["Web Address"]}
-                </Text>
-              </Text>
-            )}
-            {data[organisation]["Phone Number"] === ""
-              ? null
-              : data[organisation]["Phone Number"]
-                  .split("/")
-                  .map((num) => num.trim())
-                  .map((num) => {
-                    return (
-                      <Text
-                        key={"phone" + organisation}
-                        style={styles.flexContainer}
-                        onPress={() => {
-                          if (Platform.OS === "android") {
-                            Linking.openURL(`tel:${num.replace(/\D/g, "")}`);
-                          } else {
-                            Linking.openURL(
-                              `telprompt:${num.replace(/\D/g, "")}`
-                            );
-                          }
-                        }}
-                      >
-                        <FontAwesome style={styles.iconText} name="phone" />
-                        <Text style={styles.textBox}>{` `}</Text>
-                        <Text style={styles.textBoxPhone}>{`${num}`}</Text>
-                      </Text>
-                    );
-                  })}
-            <Pressable
-              style={styles.nextButton}
-              onPress={() => {
-                navigation.navigate("OrganisationDetailsScreen", {
-                  org: organisation,
-                });
-              }}
-            >
-              <Text style={styles.nextButtonText}>
-                <FontAwesome style={styles.nextButtonText} name="angle-right" />
-                <FontAwesome style={styles.nextButtonText} name="angle-right" />
-              </Text>
-            </Pressable>
           </View>
         ))}
       </ScrollView>
@@ -216,22 +93,26 @@ const styles = StyleSheet.create({
   image: {
     height: "100%",
   },
+  im: {
+    height: 70,
+    width: 70,
+  },
   button: {
-    backgroundColor: "#E4C9E5",
-    height: 50,
+    backgroundColor: "white",
+    height: 70,
+    width: 70,
   },
   buttonText: {
     color: "#8A449D",
     fontSize: normalize(22),
   },
   buttonContainer: {
-    marginBottom: 50,
+    alignItems: "center",
+    margin: 8,
   },
   container: {
-    flexGrow: 1,
-    margin: 8,
-    borderRadius: 5,
-    padding: "3%",
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
 });
 
