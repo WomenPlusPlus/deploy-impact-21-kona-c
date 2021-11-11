e0541eca75ee1e332002c598ef63fc738282f587
Natayra Pacheco dos Santos <natayras@gmail.com>
2021-11-11 18:41:31 +0100

    SDGs screen

--- Frontend/navigation/screens/sdgScreen.js
+++ Frontend/navigation/screens/sdgScreen.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+  View,
+  Pressable,
+} from "react-native";
+import normalize from "react-native-normalize";
+import data from "../../assets/organisations.json";
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
-const DATA = [
+const sdgs = [
   {
     id: 1,
     title: "1. No poverty",
-    photo: "https://i2.wp.com/www.un.org/sustainabledevelopment/wp-content/uploads/2017/10/E_SDG-goals_Goal-01.png?resize=1024%2C1024&ssl=1", 
-    description: "End poverty in all forms ever", 
   },
   {
     id: 2,
@@ -51,7 +67,7 @@ const DATA = [
   },
   {
     id: 12,
-    title: "12.Responsible consumption and production",
+    title: "12. Responsible consumption and production",
   },
   {
     id: 13,
@@ -59,7 +75,7 @@ const DATA = [
   },
   {
     id: 14,
-    title: "14.Life below water",
+    title: "14. Life below water",
   },
   {
     id: 15,
@@ -75,56 +91,148 @@ const DATA = [
   },
 ];
 
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-   
-      
-      <Text style={[styles.title, textColor]}>{item.title}</Text>
-  
-    
-  </TouchableOpacity>
-);
+const SdgScreen = () => {
+  const [sdgId, setSdgId] = useState(null);
+  let newData = Object.keys(data).filter((orgId) => {
+    for (let i = 0; i < data[orgId]["SDGs"].split(",").length; i++) {
+      let a = data[orgId]["SDGs"].split(",").filter((n) => n)[i];
+      if (a !== undefined) {
+        if (parseInt(a.replace(/\D/g, "")) === sdgId) {
+          return true;
+        }
+      }
+    }
+  });
+  console.log(newData);
 
-const sdgScreen = () => {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#A169B1" : "#E4C9E5";
-    const color = item.id === selectedId ? 'white' : '#921CB1';
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
+  const handlePress = (sdgId) => {
+    setSdgId(sdgId);
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-      />
-    </SafeAreaView>
+    <ImageBackground
+      source={require("../../assets/background.png")}
+      resizeMode="cover"
+      style={styles.image}
+    >
+      <ScrollView>
+        {sdgs.map((sdg) => (
+          <View style={styles.container}>
+            <Pressable
+              style={styles.button}
+              onPress={() => handlePress(sdg.id)}
+            >
+              <Text style={styles.buttonText}>{sdg.title}</Text>
+            </Pressable>
+          </View>
+        ))}
+        {newData.map((organisation) => (
+          <View key={"listview" + organisation} style={styles.container}>
+            <Text key={"name" + organisation} style={styles.textBoxName}>
+              {data[organisation]["Name of Organisation"]}
+            </Text>
+            {data[organisation]["Address"] === "" ? null : (
+              <Text key={"address" + organisation} style={styles.flexContainer}>
+                <Ionicons style={styles.iconText} name="home" />
+                <Text
+                  style={styles.textBox}
+                >{` ${data[organisation]["Address"]}`}</Text>
+              </Text>
+            )}
+            <Text key={"region" + organisation} style={styles.flexContainer}>
+              <Ionicons style={styles.iconText} name="location" />
+              <Text style={styles.textBox}>
+                {data[organisation]["Region"] === ""
+                  ? `Worldwide`
+                  : ` ${data[organisation]["Region"]}`}
+              </Text>
+            </Text>
+            {data[organisation]["Web Address"] === "" ? null : (
+              <Text key={"website" + organisation} style={styles.flexContainer}>
+                <MaterialCommunityIcons style={styles.iconText} name="web" />
+                <Text style={styles.textBox}>{` `}</Text>
+                <Text
+                  style={styles.textBoxWebsite}
+                  onPress={() =>
+                    Linking.openURL(data[organisation]["Web Address"])
+                  }
+                >
+                  {data[organisation]["Web Address"]}
+                </Text>
+              </Text>
+            )}
+            {data[organisation]["Phone Number"] === ""
+              ? null
+              : data[organisation]["Phone Number"]
+                  .split("/")
+                  .map((num) => num.trim())
+                  .map((num) => {
+                    return (
+                      <Text
+                        key={"phone" + organisation}
+                        style={styles.flexContainer}
+                        onPress={() => {
+                          if (Platform.OS === "android") {
+                            Linking.openURL(`tel:${num.replace(/\D/g, "")}`);
+                          } else {
+                            Linking.openURL(
+                              `telprompt:${num.replace(/\D/g, "")}`
+                            );
+                          }
+                        }}
+                      >
+                        <FontAwesome style={styles.iconText} name="phone" />
+                        <Text style={styles.textBox}>{` `}</Text>
+                        <Text style={styles.textBoxPhone}>{`${num}`}</Text>
+                      </Text>
+                    );
+                  })}
+            <Pressable
+              style={styles.nextButton}
+              onPress={() => {
+                navigation.navigate("OrganisationDetailsScreen", {
+                  org: organisation,
+                });
+              }}
+            >
+              <Text style={styles.nextButtonText}>
+                <FontAwesome style={styles.nextButtonText} name="angle-right" />
+                <FontAwesome style={styles.nextButtonText} name="angle-right" />
+              </Text>
+            </Pressable>
+          </View>
+        ))}
+      </ScrollView>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
   item: {
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
   },
-  
+  image: {
+    height: "100%",
+  },
+  button: {
+    backgroundColor: "#E4C9E5",
+    height: 50,
+  },
+  buttonText: {
+    color: "#8A449D",
+    fontSize: normalize(22),
+  },
+  buttonContainer: {
+    marginBottom: 50,
+  },
+  container: {
+    flexGrow: 1,
+    margin: 8,
+    borderRadius: 5,
+    padding: "3%",
+  },
 });
 
-export default sdgScreen;
+export default SdgScreen;
