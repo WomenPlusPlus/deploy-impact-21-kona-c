adcd67c02dd4e94ce82ea217c2c90ef375f98dc2
bolor <bolorchimeg.m@gmail.com>
2021-11-11 02:02:34 +0100

    updated sdg screen

--- Frontend/navigation/screens/sdgScreen.js
+++ Frontend/navigation/screens/sdgScreen.js
@@ -1,57 +1,130 @@
-import * as React from "react";
-import { View, Text, StyleSheet, ScrollView} from "react-native";
-import { styles } from "../../screens/welcomeScreen/welcomeScreenStyles";
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 
+const DATA = [
+  {
+    id: 1,
+    title: "1. No poverty",
+    photo: "https://i2.wp.com/www.un.org/sustainabledevelopment/wp-content/uploads/2017/10/E_SDG-goals_Goal-01.png?resize=1024%2C1024&ssl=1", 
+    description: "End poverty in all forms ever", 
+  },
+  {
+    id: 2,
+    title: "2. Zero hunger",
+  },
+  {
+    id: 3,
+    title: "3. Good health and well-being",
+  },
 
+  {
+    id: 4,
+    title: "4. Quality education",
+  },
+  {
+    id: 5,
+    title: "5. Gender equality",
+  },
+  {
+    id: 6,
+    title: "6. Clean water and sanitation",
+  },
+  {
+    id: 7,
+    title: "7. Affordable and clean energy",
+  },
+  {
+    id: 8,
+    title: "8. Decent work and economic growth",
+  },
+  {
+    id: 9,
+    title: "9. Industry, innovation and infrastructure",
+  },
+  {
+    id: 10,
+    title: "10. Reduced inequalities",
+  },
+  {
+    id: 11,
+    title: "11. Sustainable cities and communities",
+  },
+  {
+    id: 12,
+    title: "12.Responsible consumption and production",
+  },
+  {
+    id: 13,
+    title: "13. Climate action",
+  },
+  {
+    id: 14,
+    title: "14.Life below water",
+  },
+  {
+    id: 15,
+    title: "15. Life on land",
+  },
+  {
+    id: 16,
+    title: "16. Peace, justice and strong institutions",
+  },
+  {
+    id: 17,
+    title: "17. Partnerships for the goals",
+  },
+];
 
-export default function SdgScreen() {
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+   
+      
+      <Text style={[styles.title, textColor]}>{item.title}</Text>
   
-  const [goal, setGoal] = useState(
-    [ {goal: "No Poverty", key:1}, 
-      {goal: "Zero Hunger", key:2}, 
-      {goal: "Good health and well being", key:3}, 
-      {goal: "Quality education", key:4}, 
-      {goal: "Gender equaility", key:5}, 
-      {goal: "Clean water and sanitation", key:6}, 
-      {goal: "Affordable and clean energy", key:7}, 
-      {goal: "Decent work and economic growth", key:8}, 
-      {goal: "Industry, innovation and infrastructure", key:9}, 
-      {goal: "Reduced inequilities", key:10}, 
-      {goal: "Sustainable cities and communities", key:11}, 
-      {goal: "Responsible consumption and production", key:12}, 
-      {goal: "Climate action", key:13}, 
-      {goal: "Life below water", key:14}, 
-      {goal: "Life on land", key:15}, 
-      {goal: "Peace justice and strong institutions", key:16}, 
-      {goal: "Partnership for the goals", key:17},   
-    ]
-  ); 
+    
+  </TouchableOpacity>
+);
+
+const sdgScreen = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
 
   return (
-    <View style={stylesSDG.container}>
-      
-        <ScrollView>
-          {
-            goal.map(item =>( 
-              <View key ={item.key}>
-                <Text style ={stylesSDG.item}>{item.goal}</Text>
-              </View>
-          ))}
-        </ScrollView>
-    
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
   );
-}
+};
 
-const stylesSDG = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    paddingTop: 40,
-    paddingHorizontal: 20,
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    marginTop:24,
-    padding: 30,
-    fontSize: 24,
-  }
-}); 
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  
+});
+
+export default sdgScreen;
